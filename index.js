24238f03bd7146c338a124b6dc43fa31a59056b8
Shreyas Mohite <shreyas.mohite@emerson.com>
2024-04-19 14:47:34 +0530

    working email sending functionality

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
-
 const app = express();
-const port = 3000; // Change this to your desired port
+const port = 3000; 
 
 // Create a Nodemailer transporter
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'INETMAIL.EMRSN.NET',
+    port: 25,
+    secure: false, 
     auth: {
-        user: 'company.007.work@gmail.com',
-        pass: '**********',
-    },
-    secure: true, // Use SSL/TLS
+        user: 'shreyas.mohite@emerson.com',
+        pass: 'your-pass'
+    }
 });
 
-// Define a route to send an email
+
 app.get('/sendEmail', async (req, res) => {
     try {
         // Create email message
         const mailOptions = {
-            from: '"Company" <company.007.work@gmail.com>', // Sender address
-            to: 'shreyasmohite001@gmail.com', // Recipient address (your own email)
-            subject: 'Test Email', // Subject line
+            from: '"Software Renewal Test Mail" <shreyas.mohite@emerson.com>', // Sender address
+            to: 'parminder.singh@emerson.com', // Recipient address (your own email)
+            subject: 'Renew your Software License', // Subject line
             text: 'Hello from Nodemailer!', // Plain text body
-            html: '<b>Hello from <i>Nodemailer</i>!</b>', // HTML body
+            html: '<b>Hello from <i>Nodemailer</i>!</b></br><p>This is a test email to check the functionality of email reminder functionality!</p>', // HTML body
         };
 
         // Send the email
@@ -35,7 +35,7 @@ app.get('/sendEmail', async (req, res) => {
     }
 });
 
-// Start the server
+
 app.listen(port, () => {
-    console.log(`nodemailerProject is listening at http://localhost:${port}`);
+    console.log(`NodemailerProject is listening at http://localhost:${port}`);
 });
